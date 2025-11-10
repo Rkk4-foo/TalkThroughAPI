fab254f4dc09887175074ec31e0e4a2c1dcc7b71
Rekk4-Foo <idiotsincrasy@gmail.com>
2025-11-10 14:14:25 +0100

    Documented purpose of class

---
+++ Services/ChatService.cs
@@ -0,0 +1,27 @@
+﻿using TalkThroughAPI.Data;
+using TalkThroughAPI.DTO;
+using TalkThroughAPI.Models.Common;
+using TalkThroughAPI.Services.Interfaces;
+
+namespace TalkThroughAPI.Services
+{
+
+    /// <summary>
+    ///Handles chat-related operations
+    ///Although chat creation and management are user-driven actions
+    ///this service is i solated from its user counterpant to maintain separation of concerns
+    /// </summary>
+    public class ChatService : IChatService
+    {
+        private readonly TthroughContext _context;
+        public ChatService(TthroughContext context) 
+        {
+            _context = context;
+        }
+
+        public Task<Result<ChatDTO>> CreateChat(string currentUserId, CreateChatDTO create)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

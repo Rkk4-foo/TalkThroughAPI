072273d3bdb8148073d70b429b894a505a7d31c6
Rekk4-Foo <idiotsincrasy@gmail.com>
2025-11-10 17:58:51 +0100

    Created Extension folder and UserExtension for clean DTO creation

---
+++ Models/Extensions/UserExtension.cs
@@ -0,0 +1,27 @@
+﻿using TalkThroughAPI.DTO;
+
+namespace TalkThroughAPI.Models.Extensions
+{
+    public static class UserExtension
+    {
+        public static UserDTO toUserDTO(this User user) 
+        {
+            return new UserDTO 
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                CreationDate = user.AccountCreationDate,
+                DisplayName = user.DisplayName
+            };
+        }
+
+        public static LoginRegisterUserDTO toLoginRegisterDTO(this User user) 
+        {
+            return new LoginRegisterUserDTO 
+            {
+                UserName = user.UserName,
+                Password = user.Password,
+            };
+        }
+    }
+}

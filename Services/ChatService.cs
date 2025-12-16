28e59c17188168abce89a02f651a9dec70fea736
Rekk4-Foo <idiotsincrasy@gmail.com>
2025-12-16 21:39:01 +0100

    CreateChat method added: - Automatically creates groups and private chats depending on the context it's called in

--- Services/ChatService.cs
+++ Services/ChatService.cs
@@ -18,9 +18,11 @@ namespace TalkThroughAPI.Services
     public class ChatService : IChatService
     {
         private readonly TthroughContext _context;
-        public ChatService(TthroughContext context)
+        private readonly IWebHostEnvironment _env;
+        public ChatService(TthroughContext context, IWebHostEnvironment env)
         {
             _context = context;
+            _env = env;
         }
 
         /// <summary>
@@ -34,37 +36,69 @@ namespace TalkThroughAPI.Services
         /// <param name="create">List of users that participate in the chat and chat name.</param>
         /// <returns>Chat information and StatusCode of the request encapsulated on a return record.</returns>
 
-        public async Task<Result<ChatDTO>> CreateChat(String currentUserId, CreateChatDTO create) 
+        public async Task<Result<ChatDTO>> CreateChat(string currentUserId, CreateChatDTO create)
         {
-            throw new NotImplementedException();
+            
+            var userIds = create.UserIds.Select(u => u.Id).Distinct().ToList();
+
+            if (!userIds.Contains(currentUserId))
+                userIds.Add(currentUserId);
+
+            var users = await _context.Users
+                .Where(u => userIds.Contains(u.Id))
+                .ToListAsync();
+
+            if (users.Count != userIds.Count)
+                return Result<ChatDTO>.Failure(
+                    "Some users do not exist in DB",
+                    StatusCodes.Status400BadRequest,
+                    "USER_NON_EXISTENT"
+                );
+
+            MediaService ms = new MediaService();
+
+            var defaultPicturePath = Path.Combine(_env.ContentRootPath, "wwwroot", "Images", "DefaultPicture.png");
+            var pfp = ms.GetDefaultImageBytes(defaultPicturePath);
+
+            var chat = new Chat
+            {
+                ChatId = Guid.NewGuid().ToString(),
+                ChatCreationDate = DateTime.Now,
+                ChatPicture = ms.GetDefaultImageBytes(_env.ContentRootPath),
+                ChatName = users.Count > 2
+                    ? create.ChatName
+                    : users.First(u => u.Id != currentUserId).UserName
+            };
+
+            foreach (var user in users)
+            {
+                var userChat = new UserChat
+                {
+                    ChatId = chat.ChatId,
+                    UserId = user.Id,
+                    UserName = user.UserName,
+                    ChatName = chat.ChatName,
+                    CreatedAt = DateTime.Now
+                };
+
+                _context.UserChat.Add(userChat);
+            }
+
+            _context.Chats.Add(chat);
+            await _context.SaveChangesAsync();
+
+            return Result<ChatDTO>.SuccessR
+                (
+                    new ChatDTO
+                    {
+                        ChatId = chat.ChatId,
+                        ChatName = chat.ChatName,
+                        CreatedAt = DateTime.Now
+                    },
+                    "Chat created successfully",
+                    default
+                );
         }
-
-        //public async Task<Result<ChatDTO>> CreateChat(string currentUserId, CreateChatDTO create)
-        //{
-        //    var userIds = create.UserIds.Select(u => u.Id).Distinct().ToList();
-
-        //    if (!userIds.Contains(currentUserId))
-        //        userIds.Add(currentUserId);
-
-        //    var users = await _context.Users
-        //        .Where(u => userIds.Contains(u.Id))
-        //        .ToListAsync();
-
-        //    if (users.Count != userIds.Count)
-        //        return Result<ChatDTO>.Failure(
-        //            "Some users do not exist in DB",
-        //            StatusCodes.Status400BadRequest,
-        //            "USER_NON_EXISTANT"
-        //        );
-
-        //    if (users.Count > 2)
-        //    {
-        //        var chat = new Chat
-        //        {
-        //            ChatId = Guid.NewGuid().ToString(),
-
-        //        };
-        //    }
 
     }
 }
